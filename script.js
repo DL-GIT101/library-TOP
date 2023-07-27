f208607f70b4b41190e0b43a1bd284f14a756b2c
donlHenard <pastor.git101@gmail.com>
2023-07-27 21:17:50 +0800

    Add event to close btn

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const addBTN = document.querySelector('header > svg');
+const closeBTN = document.querySelector('.modal > .content > form > .buttons > .close');
 const modal = document.querySelector('.modal');
 
 addBTN.addEventListener('click', () => {
   modal.classList.remove('hidden');
+})
+
+closeBTN.addEventListener('click', (event) => {
+  event.preventDefault()
+  modal.classList.add('hidden');
 })
 
 let myLibrary = [];
