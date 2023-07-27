cdd88b0630a9f4f397a8f25a78296f3a8f8d3351
donlHenard <pastor.git101@gmail.com>
2023-07-27 21:44:56 +0800

    Add submit button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const addBTN = document.querySelector('header > svg');
 const closeBTN = document.querySelector('.modal > .content > form > .buttons > .close');
+const submitBTN = document.querySelector('.modal > .content > form > .buttons > .add')
 const modal = document.querySelector('.modal');
 
 addBTN.addEventListener('click', () => {
@@ -9,6 +10,10 @@ addBTN.addEventListener('click', () => {
 closeBTN.addEventListener('click', (event) => {
   event.preventDefault()
   modal.classList.add('hidden');
+})
+
+submitBTN.addEventListener('click', (event) => {
+  event.preventDefault()
 })
 
 let myLibrary = [];
