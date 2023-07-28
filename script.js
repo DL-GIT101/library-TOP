12ecd32865c916ca14db80abfff7c4bc3d69c891
donlHenard <pastor.git101@gmail.com>
2023-07-28 20:24:09 +0800

    Add sample Books obj & fn displayBook

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addBTN = document.querySelector('header > svg');
 const closeBTN = document.querySelector('.modal > .content > form > .buttons > .close');
 const submitBTN = document.querySelector('.modal > .content > form > .buttons > .add')
 const modal = document.querySelector('.modal');
+const cabinet = document.querySelector('.cabinet')
 
 addBTN.addEventListener('click', () => {
   modal.classList.remove('hidden');
@@ -15,10 +16,12 @@ closeBTN.addEventListener('click', (event) => {
 submitBTN.addEventListener('click', (event) => {
   event.preventDefault()
 })
-
-let myLibrary = [sampleBook];
+const sampleBook0 = new Book('Harry Potter', 'J.K. Rowling', 569, false);
+const sampleBook1 = new Book('Hairy Shaver', 'Joke Rollin', 420, true);
+let myLibrary = [sampleBook0,sampleBook1];
 
 function Book(title, author, pages, read) {
+
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -29,4 +32,37 @@ function addBookToLibrary() {
   // do stuff here
 }
 
-const sampleBook = new Book('Harry Potter', 'J.K. Rowling', 569, false);
+function displayBooks() {
+
+  myLibrary.forEach(book => {
+    let bookContainer = document.createElement('div');
+    bookContainer.classList.add('book');
+    cabinet.appendChild(bookContainer);
+
+    let bookTitle = document.createElement('h3');
+    bookTitle.textContent = book.title;
+    bookContainer.appendChild(bookTitle);
+
+    let bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('author');
+    bookAuthor.textContent = book.author;
+    bookContainer.appendChild(bookAuthor);
+
+    let bookPages = document.createElement('p');
+    bookPages.classList.add('pages');
+    bookPages.textContent = book.pages + ' pages';
+    bookContainer.appendChild(bookPages);
+
+    let readStatus = document.createElement('div');
+    readStatus.classList.add('toggle');
+    if(book.read === true){
+      statusMsg = "Finished";
+    }else {
+      statusMsg = "Unread";
+    }
+    readStatus.textContent = statusMsg;
+    bookContainer.appendChild(readStatus);
+  });
+}
+
+displayBooks();
